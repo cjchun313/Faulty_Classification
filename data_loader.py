fceec5328e56197577310c35bf954c39c6ea9025
cjchun313 <cjchun313@gmail.com>
2020-06-07 19:01:50 +0900

    add cuda setting

--- data_loader.py
+++ data_loader.py
@@ -4,17 +4,17 @@ from torch.utils.data import DataLoader
 def data_loader(train=True, batch_size=64, shuffle=True, num_workers=0):
     composed = transforms.Compose([transforms.ToTensor()])
     if train is True:
-        dataset = datasets.ImageFolder(root='./db/data_source/train/', transform=composed)
+        dataset = datasets.ImageFolder(root='../db/data_source/train/', transform=composed)
     else:
-        dataset = datasets.ImageFolder(root='./db/data_source/val/', transform=composed)
+        dataset = datasets.ImageFolder(root='../db/data_source/val/', transform=composed)
 
     dl = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
     return dl
 
 if __name__ == "__main__":
     composed = transforms.Compose([transforms.ToTensor()])
-    train_dataset = datasets.ImageFolder(root='./db/data_source/train/', transform=composed)
-    val_dataset = datasets.ImageFolder(root='./db/data_source/val/', transform=composed)
+    train_dataset = datasets.ImageFolder(root='../db/data_source/train/', transform=composed)
+    val_dataset = datasets.ImageFolder(root='../db/data_source/val/', transform=composed)
     print(train_dataset, val_dataset)
 
     train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=0)
--- train.py
+++ train.py
@@ -9,11 +9,12 @@ from data_loader import data_loader
 from models.resnet import ResNet18
 
 
-def train(model, train_loader, optimizer, epoch):
+def train(model, train_loader, optimizer, epoch, DEVICE):
     model.train()
 
-    for batch_idx, samples in tqdm(enumerate(train_loader)):
+    for batch_idx, samples in enumerate(train_loader):
         data, target = samples
+        data, target = data.to(DEVICE), target.to(DEVICE)
 
         output = model(data)
         loss = F.cross_entropy(output, target)
@@ -25,14 +26,14 @@ def train(model, train_loader, optimizer, epoch):
     print('Epoch: {}\tLoss: {:.6f}'.format(epoch, loss.item()))
 
 
-def evaluate(model, test_loader):
+def evaluate(model, test_loader, DEVICE):
     model.eval()
     test_loss = 0
     correct = 0
 
     with torch.no_grad():
         for data, target in test_loader:
-            data, target = data, target
+            data, target = data.to(DEVICE), target.to(DEVICE)
             output = model(data)
             test_loss += F.cross_entropy(output, target, reduction ='sum').item()
             prediction = output.max(1, keepdim = True)[1]
@@ -48,14 +49,15 @@ def main(args):
 
     USE_CUDA = torch.cuda.is_available()
     DEVICE = torch.device('cuda' if USE_CUDA else 'cpu')
+    print('device:', DEVICE)
 
     model = ResNet18().to(DEVICE)
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
     acc_prev = 0
-    for epoch in range(args.epoch + 1):
-        train(model, train_loader, optimizer, epoch)
-        loss, acc = evaluate(model, val_loader)
+    for epoch in tqdm(range(args.epoch + 1)):
+        train(model, train_loader, optimizer, epoch, DEVICE)
+        loss, acc = evaluate(model, val_loader, DEVICE)
         if acc > acc_prev:
             acc_prev = acc
 
