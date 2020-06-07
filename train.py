6959981487aebba5a92bb5aea0fbf40e0e798ebc
cjchun313 <cjchun313@gmail.com>
2020-06-07 18:23:23 +0900

    cnn-resnet classification

---
+++ train.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from tqdm import tqdm
+
+import argparse
+
+from data_loader import data_loader
+from models.resnet import ResNet18
+
+
+def train(model, train_loader, optimizer, epoch):
+    model.train()
+
+    for batch_idx, samples in tqdm(enumerate(train_loader)):
+        data, target = samples
+
+        output = model(data)
+        loss = F.cross_entropy(output, target)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    print('Epoch: {}\tLoss: {:.6f}'.format(epoch, loss.item()))
+
+
+def evaluate(model, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data, target
+            output = model(data)
+            test_loss += F.cross_entropy(output, target, reduction ='sum').item()
+            prediction = output.max(1, keepdim = True)[1]
+            correct += prediction.eq(target.view_as(prediction)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    test_accuracy = 100. * correct / len(test_loader.dataset)
+    return test_loss, test_accuracy
+
+def main(args):
+    train_loader = data_loader(train=True, batch_size=args.batch_size)
+    val_loader = data_loader(train=False)
+
+    USE_CUDA = torch.cuda.is_available()
+    DEVICE = torch.device('cuda' if USE_CUDA else 'cpu')
+
+    model = ResNet18().to(DEVICE)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    acc_prev = 0
+    for epoch in range(args.epoch + 1):
+        train(model, train_loader, optimizer, epoch)
+        loss, acc = evaluate(model, val_loader)
+        if acc > acc_prev:
+            acc_prev = acc
+
+            savePath = './pth/resnet_model-{:4d}-{:.4f}.pth'.format(epoch, loss)
+            torch.save(model.state_dict(), savePath)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--batch_size',
+        help='the number of samples in mini-batch',
+        default=64,
+        type=int)
+    parser.add_argument(
+        '--epoch',
+        help='number of training iterations',
+        default=1000,
+        type=int)
+
+    args = parser.parse_args()
+    print(args)
+
+    main(args)
